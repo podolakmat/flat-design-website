409aaf12f88c98b26ab18128a0e1c1551651f5da
Mateusz Podolak <podolakmateusz@outlook.com>
2018-12-29 14:59:43 +0100

    Created nav/menu and logo - mobile menu, sticky menu after scrolling. Added logo image and js files (with menu script)

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var n = '#nav__wrapper--list',
+        no = 'nav__open';
+    $('.nav__wrapper--hamburger').click(function() {
+        if ($(n).hasClass(no)) {
+
+            $(n).animate({
+                height: 0
+            }, 300);
+            setTimeout(function() {
+                $(n).removeClass(no).removeAttr('style');
+            }, 320);
+        } else {
+            var newH = $(n).css('height', 'auto').height();
+            $(n).height(0).animate({
+                height: newH
+            }, 300);
+            setTimeout(function() {
+                $(n).addClass(no).removeAttr('style');
+            }, 320);
+        }
+    });
+
+
+    var stickyOffset = $('#nav').offset().top;
+
+    $(window).scroll(function() {
+        var nav = $('#nav'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= stickyOffset) nav.addClass('sticky');
+        else nav.removeClass('sticky');
+    });
+
+
+
+});
